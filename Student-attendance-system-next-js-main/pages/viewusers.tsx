4fdcb90f4b7262ebb18308bddbc9f752a049dbab
Amirali Hashemipoor <113814815+amirzerok@users.noreply.github.com>
2025-02-23 02:39:49 +0330

    Update viewusers.tsx

--- Student-attendance-system-next-js-main/pages/viewusers.tsx
+++ Student-attendance-system-next-js-main/pages/viewusers.tsx
@@ -68,7 +68,7 @@ const UsersTable: React.FC = () => {
       try {
         const [usersResponse, rolesResponse] = await Promise.all([
           axios.get('/api/users'),
-          axios.get('http://localhost:3001/users/role'),
+          axios.get('http://nestjs:3001/users/role'),
         ]);
         setUsers(usersResponse.data);
         setRoles(rolesResponse.data);
