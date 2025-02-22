7a26ce59bc76d5e6301d58c6268505c3f5f6f38a
Amirali Hashemipoor <113814815+amirzerok@users.noreply.github.com>
2025-02-23 02:47:32 +0330

    Update camera.tsx

--- Student-attendance-system-next-js-main/pages/camera/camera.tsx
+++ Student-attendance-system-next-js-main/pages/camera/camera.tsx
@@ -24,7 +24,7 @@ const CameraComponent: React.FC = () => {
       }
 
       try {
-        const response = await fetch('http://127.0.0.1:5000/upload', {
+        const response = await fetch('http://pythonserver:5000/upload', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
