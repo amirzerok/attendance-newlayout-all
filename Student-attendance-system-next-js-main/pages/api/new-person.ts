da04385de2e33fb533163fd6cede0c6805738451
Amirali Hashemipoor <113814815+amirzerok@users.noreply.github.com>
2025-02-23 12:55:03 +0330

    Update new-person.ts

--- Student-attendance-system-next-js-main/pages/api/new-person.ts
+++ Student-attendance-system-next-js-main/pages/api/new-person.ts
@@ -38,7 +38,7 @@ export default async (req: RequestWithFiles, res: NextApiResponse) => {
         const face = files && files.length > 0 ? files[0].buffer : null;
 
         const response = await axios.post(
-          'http://nestjs:3001/new-person',
+          'http://91.107.186.94:3001/new-person',
           {
             face,
             firstName,
