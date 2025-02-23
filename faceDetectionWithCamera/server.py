1c05a952bd97f3388f71db0be87d9560885f2a09
Amirali Hashemipoor <113814815+amirzerok@users.noreply.github.com>
2025-02-23 11:31:35 +0330

    Update server.py

--- faceDetectionWithCamera/server.py
+++ faceDetectionWithCamera/server.py
@@ -200,4 +200,4 @@ def upload_image():
         return jsonify({"status": "error", "message": "خطا در پردازش تصویر"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
