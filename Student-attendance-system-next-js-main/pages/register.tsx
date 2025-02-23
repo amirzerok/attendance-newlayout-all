9df2719cb96b56326dc4d8a71bb759380f76ee11
Amirali Hashemipoor <113814815+amirzerok@users.noreply.github.com>
2025-02-23 12:57:28 +0330

    Update register.tsx

--- Student-attendance-system-next-js-main/pages/register.tsx
+++ Student-attendance-system-next-js-main/pages/register.tsx
@@ -69,7 +69,7 @@ const RegisterPage = () => {
   useEffect(() => {
     const fetchRoles = async () => {
       try {
-        const response = await axios.get('http://nestjs:3001/users/role');
+        const response = await axios.get('http://91.107.186.94:3001/users/role');
         setRoles(response.data);
       } catch (error) {
         setAlert({
